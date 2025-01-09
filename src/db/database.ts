63be9c577bb723c6b15f2678b98c36562f40c248
rezarayan <rezarayan78@gmail.com>
2025-01-09 19:40:52 +0330

    ready to deploy

--- src/db/database.ts
+++ src/db/database.ts
@@ -3,11 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 export const database = TypeOrmModule.forRoot({
   type: 'postgres',
-  host: 'localhost',
+  host: 'mafia-platform-aza-service',
   port: Number(process.env.DB_PORT) || 5432,
   username: 'postgres',
-  password: '2wsx3edc',
-  database: 'mafia-platform',
+  password: 'd1uNnME6kv6rseSJwXPd',
+  database: 'mafia-plbfo_db',
   autoLoadEntities: true,
   synchronize: true,
 });
